f8ffb99e18214cd02169788b7e40b36e398e3552
Kushagra Gupta <kushagra.2021@tecorb.co>
2022-09-29 12:13:08 +0530

    new update

--- app/controllers/api/v1/payments_controller.rb
+++ app/controllers/api/v1/payments_controller.rb
@@ -6,40 +6,6 @@ class Api::V1::PaymentsController < ApplicationController
 		begin
 			amount = params[:amount].to_i * 100
 			@order = Payments::Create.call(@user, params)
-			Razorpay.setup(ENV["RAZORPAY_KEY_ID"],ENV["RAZORPAY_KEY_SECRET"])
-			Razorpay.headers = {"Content-type" => "application/json"}
-
-			para_attr = {"amount": amount,"currency": "INR","receipt": "#{@order.id}"}
-
-			order = Razorpay::Order.create(para_attr.to_json)
-					
-			@order.update(gateway_order_id: order.id)
-
-			Razorpay.headers = {"Content-type" => "application/json"}
-
-			para_attr = {
-			 "amount": amount,
-			 "currency": "INR",
-			 "accept_partial": false,
-			 "first_min_partial_amount": 100,
-								  "description": "Restaurant Order",
-								  "customer": {
-								    "name": @user.name,
-								    "email": @user.email,
-								    "contact": @user.contact
-								  },
-								  "notify": {
-								    "sms": true,
-								    "email": true
-								  },
-								  "reminder_enable": true,
-								  "callback_url": "https://example-callback-url.com/",
-								  "callback_method": "get"
-						}
-
-			payment_link = Razorpay::PaymentLink.create(para_attr.to_json)
-					
-			@order.update(Paylink_id: payment_link.id,payment_link:payment_link.short_url)
 								
 			render :json=>{code:200,message:"success",order:@order}
 		rescue Exception => e
--- app/services/payments/create.rb
+++ app/services/payments/create.rb
@@ -10,6 +10,40 @@ class Payments::Create < ApplicationService
         user_id: @user.id,
         amount: params[:amount]
       })
+      Razorpay.setup(ENV["RAZORPAY_KEY_ID"],ENV["RAZORPAY_KEY_SECRET"])
+      Razorpay.headers = {"Content-type" => "application/json"}
+
+      para_attr = {"amount": amount,"currency": "INR","receipt": "#{@order.id}"}
+
+      order = Razorpay::Order.create(para_attr.to_json)
+          
+      @order.update(gateway_order_id: order.id)
+
+      Razorpay.headers = {"Content-type" => "application/json"}
+
+      para_attr = {
+       "amount": amount,
+       "currency": "INR",
+       "accept_partial": false,
+       "first_min_partial_amount": 100,
+                  "description": "Restaurant Order",
+                  "customer": {
+                    "name": @user.name,
+                    "email": @user.email,
+                    "contact": @user.contact
+                  },
+                  "notify": {
+                    "sms": true,
+                    "email": true
+                  },
+                  "reminder_enable": true,
+                  "callback_url": "https://example-callback-url.com/",
+                  "callback_method": "get"
+            }
+
+      payment_link = Razorpay::PaymentLink.create(para_attr.to_json)
+          
+      @order.update(Paylink_id: payment_link.id,payment_link:payment_link.short_url)
   end
 
   def call
