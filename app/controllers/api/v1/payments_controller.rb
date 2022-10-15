d32562c43a46dcbc37ec61ad98569ed866a8cd0e
Jai <jai@tecorb.co>
2022-10-15 11:55:09 +0530

    Comments added in order and payments verify requests

--- app/controllers/api/v1/payments_controller.rb
+++ app/controllers/api/v1/payments_controller.rb
@@ -2,6 +2,9 @@ class Api::V1::PaymentsController < ApplicationController
 	before_action :check_logged_user
 
 	def verify_payment
+		# Request will receive here after payment status update (completed/failed)
+		# This is working like as webhook in which Razoropay signature of payment will be received
+		# Signature will be verified from razorpay and order will proceed on the bases of response
 		begin
 			@order = Order.where(gateway_order_id: params[:gateway_order_id]).first
 			if @order.present?
